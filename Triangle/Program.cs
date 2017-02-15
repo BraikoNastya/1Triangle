a395a2d26cdfcdeaa5e322fb63653a2a0611b913
BraikoNastya <lily9866@mail.ru>
2017-02-16 02:08:08 +0500

    add class point and add RandomCoordinate

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace Triangle
 {
-    class Program
+    class Point
     {
-        static void Main(string[] args)
+        public int coordinateX;
+        public int coordinateY;
+
+        public Point(int coordinateX, int coordinateY)
         {
+            this.coordinateX = coordinateX;
+            this.coordinateY = coordinateY;
         }
     }
+    class Program
+    {
+        public static void RandomCoordinate(Point [] points )
+        {
+            Random gen = new Random();
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point(gen.Next(1, 15), gen.Next(1, 15));
+                Console.WriteLine("ваши точки x={0} и y={1}" , points[i].coordinateX, points[i].coordinateY);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Point[] points = new Point[3];
+            RandomCoordinate(points);
+
+            Console.ReadKey();
+        }
+        
+    }
+
 }
